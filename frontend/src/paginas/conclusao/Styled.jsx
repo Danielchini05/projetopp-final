2ee4c56674da4bb4a3bcb53fbe4d13d5b8cec1c8
Daniel Chini <danielchini03@gmail.com>
2023-11-17 12:19:25 -0300

    atualização

--- frontend/src/paginas/conclusao/Styled.jsx
+++ frontend/src/paginas/conclusao/Styled.jsx
@@ -1,6 +1,15 @@
 import { styled } from "styled-components"
 
 export const ConteudoConclusao = styled.div`
+    display: flex;
     justify-content: center;
+    align-items: center;
+    padding-top: 9vh;
+    padding-bottom: 33vh;
     margin: 0 20% 0 20%;
+
+    p {
+        text-align: justify;
+    }
 `
+
--- frontend/src/paginas/home/Home.js
+++ frontend/src/paginas/home/Home.js
@@ -20,12 +20,20 @@ function Home(){
             </TituloHome>
 
             <p>
-            A produção e compra de carros elétricos vem aumentando ao passar dos anos e a falta de estrutura para o abastecimento desses veículos é um problema. As pessoas que compra um carro elétrico tem a opção de comprar um carregador para sua residência, no entanto às vezes em alguns casos é necessário o uso de postos públicos que muitas das vezes são difíceis de localizar devido à falta de postos para carregamento de carros elétricos. De acordo com a ABVE, há apenas 1.250 pontos de recarga no país, sendo que 47% deles estão em São Paulo. Algumas empresas de carregadores para veículos elétricos já entraram em parceria com shoppings de bairros mais nobres, portanto o uso dos carros elétricos tende a aumentar junto com a busca por postos de carregamento público para uma recarga rápida.  
+                Os carros elétricos, fruto de uma constante evolução tecnológica, emergem como protagonistas na busca por uma mobilidade mais sustentável. Seus benefícios ambientais são inegáveis, com a redução significativa das emissões de gases de efeito estufa, contribuindo para mitigar as mudanças climáticas. Além disso, a eficiência energética dos motores elétricos e os custos operacionais mais baixos proporcionam uma perspectiva econômica atrativa para os consumidores.
+
+                No entanto, os desafios persistem. A autonomia limitada das baterias e a ainda em desenvolvimento infraestrutura de recarga são aspectos que demandam atenção contínua. O custo inicial mais elevado também figura como uma barreira à adoção em massa. Contudo, governos ao redor do mundo têm respondido a esse desafio com incentivos fiscais e subsídios para impulsionar a transição para os veículos elétricos.  
             </p>
 
             <ImgConteudo>
                 <img src={imagemConteudo}/>
             </ImgConteudo>
+
+            <p>
+                Além dos aspectos econômicos, a produção e descarte de baterias levantam preocupações ambientais. A mineração de materiais como lítio e cobalto, essenciais na fabricação das baterias de íon de lítio, demanda uma abordagem cuidadosa para minimizar seus impactos negativos. A busca por métodos sustentáveis de produção e reciclagem de baterias é uma área crucial de pesquisa e desenvolvimento.
+
+                Assim, enquanto os carros elétricos prometem um futuro mais limpo e eficiente, é fundamental reconhecer tanto seus benefícios quanto desafios. O compromisso contínuo com a inovação, aliado a políticas públicas favoráveis, é essencial para acelerar a transição para uma frota de veículos mais sustentável e moldar um cenário onde a mobilidade elétrica seja acessível e ambientalmente responsável.
+            </p>
 
         </ConteudoHome>
 
